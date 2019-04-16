46589718d257bc9f738cee5c5cc8f9e5021d6ec0
ludorum <soosmail@me.com>
2019-04-16 17:08:03 +0900

    setup first.

---
+++ core/site.js
@@ -0,0 +1,146 @@
+const config = {};
+StellarSdk.Network.usePublicNetwork();
+async function readyForBlockchain() {
+    $("#loading").lock();
+    config.server = new StellarSdk.Server('https://horizon.stellar.org');
+    config.web3 = new Web3('https://rinkeby.infura.io');
+
+    config.assetXLM = StellarSdk.Asset.native();
+    config.assetXLMC = new StellarSdk.Asset("XLMC", "GCCZALMK4YG44CUKMOXQZAXNQDSTFW6T7EIDZANGIGOJDSYU62B4UXRD");
+    config.assetKRW = new StellarSdk.Asset("KRW", "GCCZALMK4YG44CUKMOXQZAXNQDSTFW6T7EIDZANGIGOJDSYU62B4UXRD");
+
+    config.platformKey = StellarSdk.Keypair.fromSecret("SDLOQYBEYBPUCZMWSYC275TQGGN7FG6JEQBSWMAOCRYDK5INFEXFOK5R");
+    config.collateralKey = StellarSdk.Keypair.fromSecret("SAMPEKPCJYGDRUIWSLCGTK3TD47NNNMQJJXMQTDKMIVKHLB7FKPTFDHA");
+
+    config.fee = await config.server.fetchBaseFee();
+    await refreshBlockchain();
+}
+
+async function refresh() {
+    if ($("#loading").lock()) {
+        return;
+    }
+    $("#loading_mask").show();
+    await refreshBlockchain();
+    resultMessage("done");
+}
+async function refreshBlockchain() {
+    resultMessage("Ready");
+    const [platform, collateral, operations] = await Promise.all([
+        config.server.loadAccount(config.platformKey.publicKey()),
+        config.server.loadAccount(config.collateralKey.publicKey()),
+        config.server.operations().forAccount(config.collateralKey.publicKey()).limit(20).order("desc").call()
+    ])
+    config.platform = platform
+    config.collateral = collateral
+    const min = config.collateral.subentry_count * 0.5 + 1;
+    $("#user_xlm").text((Number(findBalance(collateral, config.assetXLM).balance) - min) + config.assetXLM.code)
+    $("#user_xlmc").text((Number(findBalance(collateral, config.assetXLMC).balance)) + config.assetXLMC.code)
+    $("#user_krw").text((Number(findBalance(collateral, config.assetKRW).balance)) + config.assetKRW.code)
+
+    $("#history").empty();
+    operations.records.forEach(function(row) {
+        if (row.type == "payment") {
+            let asset = null;
+            if (row.asset_type == "native") {
+                asset = config.assetXLM;
+            } else {
+                asset = new StellarSdk.Asset(row.asset_code, row.asset_issuer)
+            }
+            let direction = row.from == config.collateral.id ? "Send" : "Recieve";
+            var $div = $("<div/>", {
+                "class": "hisotry-row"
+            }).text(`${direction} ${row.amount} ${asset.code}`);
+            $div.on('click', function() {
+                window.open(`https://stellar.expert/explorer/public/tx/${row.transaction_hash}`)
+            })
+            $div.appendTo($("#history"));
+        }
+    })
+    console.log(operations.records);
+}
+async function transXLMtoXLMC() {
+    const min = config.collateral.subentry_count * 0.5 + 1;
+    const amount = Number(findBalance(config.collateral, config.assetXLM).balance) - min;
+    if (amount <= 0) {
+        return;
+    }
+    if ($("#loading").lock()) {
+        return;
+    }
+    $("#loading_mask").show();
+    const builder = new StellarSdk.TransactionBuilder(config.platform, {
+        fee: config.fee
+    }).addOperation(StellarSdk.Operation.payment({
+        amount: amount + "",
+        destination: config.platform.id,
+        asset: config.assetXLM,
+        source: config.collateral.id,
+    })).addOperation(StellarSdk.Operation.payment({
+        amount: amount + "",
+        destination: config.collateral.id,
+        asset: config.assetXLMC,
+        source: config.platform.id,
+    }));
+    const tx = builder.setTimeout(100).build();
+    tx.sign(config.platformKey)
+    await config.server.submitTransaction(tx);
+    await refreshBlockchain();
+    resultMessage("done")
+}
+async function transXLMCtoXLM() {
+    const amount = Number(findBalance(config.collateral, config.assetXLMC).balance);
+    if (amount <= 0) {
+        return;
+    }
+    if ($("#loading").lock()) {
+        return;
+    }
+    $("#loading_mask").show();
+    const builder = new StellarSdk.TransactionBuilder(config.platform, {
+        fee: config.fee
+    }).addOperation(StellarSdk.Operation.payment({
+        amount: amount + "",
+        destination: config.collateral.id,
+        asset: config.assetXLM,
+        source: config.platform.id,
+    })).addOperation(StellarSdk.Operation.payment({
+        amount: amount + "",
+        destination: config.platform.id,
+        asset: config.assetXLMC,
+        source: config.collateral.id,
+    })).addOperation(StellarSdk.Operation.payment({
+        amount: (amount * 10000) + "",
+        destination: config.platform.id,
+        asset: config.assetKRW,
+        source: config.collateral.id,
+    }));
+    const tx = builder.setTimeout(100).build();
+    tx.sign(config.platformKey)
+    await config.server.submitTransaction(tx);
+    await refreshBlockchain();
+    resultMessage("done")
+}
+function findBalance(account, asset) {
+    const balance = account.balances.find(item => {
+        switch (item.asset_type) {
+            case "credit_alphanum4":
+            case "credit_alphanum12":
+                if (item.asset_code == asset.code
+                    && item.asset_issuer == asset.issuer) {
+                    return true;
+                }
+                break;
+            case "native":
+                if (asset.issuer == undefined) {
+                    return true;
+                }
+        }
+        return false;
+    })
+    return balance;
+}
+function resultMessage(message) {
+    $("#loading").unlock();
+    $("#loading_mask").fadeOut(300);
+}
---
+++ core/tmpl.js
@@ -0,0 +1,166 @@
+((function($) {
+    $.fn.extend({
+        isLock : function() {
+            if (this.hasClass('wait-lock')) {
+                return true;
+            }
+        },
+        lock : function() {
+            if (this.isLock()) {
+                return true;
+            }
+            this.addClass('wait-lock');
+            return false;
+        },
+        enter : function(action) {
+          $.each(this, function() {
+            $(this).on('keypress', function(e) {
+              if (e.which == 13) {
+                action.apply(this);
+              }
+            })
+          })
+          return this;
+        },
+        unlock : function() {
+            this.removeClass('wait-lock');
+        },
+        serializeObject : function() {
+            var o = {};
+            var a = this.serializeArray();
+            $.each(a, function() {
+                if (o[this.name] !== undefined) {
+                    if (!o[this.name].push) {
+                        o[this.name] = [o[this.name]];
+                    }
+                    o[this.name].push(this.value || '');
+                } else {
+                    o[this.name] = this.value || '';
+                }
+            });
+            return o;
+        }
+    });
+    $.number_format = function(number) {
+        return (number+'').replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+    $.api = function(method, url, data, option) {
+        if (data instanceof FormData) {
+            option = $.extend({
+                processData : false,
+                contentType : false
+            }, option);
+        }
+        var option = $.extend({
+            'url' : url,
+            'method' : method,
+            'data' : data,
+            'dataType' : 'json',
+            'error' : function(xhr, status, error) {
+                if (xhr.status == 0) {
+                    return;
+                }
+                console.log(xhr);
+                alert(error);
+            }
+        }, option);
+        return $.ajax(option);
+    }
+    $.input = function(key, url) {
+        if (!url) {
+          url = window.location.href;
+        }
+        key = key.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + key + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    $.response = function(response, fn, fail) {
+        if (response.code >= 200 && response.code < 300) {
+            fn(response.data, response.code, response.message);
+        } else {
+            if (fail) {
+                fail(response.message, response.code, response.data);
+            } else {
+                alert(response.message);
+            }
+        }
+    }
+    $.task = function(task) {
+        var defer = new $.Deferred();
+        var when = $.when(defer);
+        task(defer);
+        return when;
+    }
+    $.seed = function(len) {
+        if (!len) {
+            len = 8;
+        }
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( var i=0; i < len; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+})(jQuery));
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+(function(){
+  var cache = {};
+  this.tmpl = function tmpl(str, data){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/[^a-zA-Z0-9 \-_]/.test(str) ?
+      cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :
+
+      // Generate a reusable function that will serve as a template
+      // generator (and which will be cached).
+      new Function("obj",
+        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+        // Introduce the data as local variables using with(){}
+        "with(obj){p.push('" +
+
+        // Convert the template into pure JavaScript
+        str
+          .replace(/[\r\t\n]/g, " ")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("\t").join("');")
+          .split("%>").join("p.push('")
+          .split("\r").join("\\'")
+      + "');}return p.join('');");
+    // Provide some basic currying to the user
+    return data ? fn( data ) : fn;
+  };
+  this.loadTmplScript = function loadTmplScript(url) {
+    var key = md5(url);
+    if ($('body').data(key) === true) {
+      return $.task(function() {
+        task.resolve();
+      });
+    } else {
+      return $.ajax({
+        url:url,
+        type:'GET'
+      }).pipe(function(response) {
+        $('body').data(key, true);
+        var $hidden =  $('<div id="roaded-script-'+key+'"/>').html(response);
+        $('body').append($hidden);
+        $hidden.hide();
+      });
+    }
+  }
+})();
+/**
+  * MD5
+  * https://github.com/blueimp/JavaScript-MD5
+  * The JavaScript MD5 script is released under the MIT license.
+  */
+!function(a){"use strict";function b(a,b){var c=(65535&a)+(65535&b),d=(a>>16)+(b>>16)+(c>>16);return d<<16|65535&c}function c(a,b){return a<<b|a>>>32-b}function d(a,d,e,f,g,h){return b(c(b(b(d,a),b(f,h)),g),e)}function e(a,b,c,e,f,g,h){return d(b&c|~b&e,a,b,f,g,h)}function f(a,b,c,e,f,g,h){return d(b&e|c&~e,a,b,f,g,h)}function g(a,b,c,e,f,g,h){return d(b^c^e,a,b,f,g,h)}function h(a,b,c,e,f,g,h){return d(c^(b|~e),a,b,f,g,h)}function i(a,c){a[c>>5]|=128<<c%32,a[(c+64>>>9<<4)+14]=c;var d,i,j,k,l,m=1732584193,n=-271733879,o=-1732584194,p=271733878;for(d=0;d<a.length;d+=16)i=m,j=n,k=o,l=p,m=e(m,n,o,p,a[d],7,-680876936),p=e(p,m,n,o,a[d+1],12,-389564586),o=e(o,p,m,n,a[d+2],17,606105819),n=e(n,o,p,m,a[d+3],22,-1044525330),m=e(m,n,o,p,a[d+4],7,-176418897),p=e(p,m,n,o,a[d+5],12,1200080426),o=e(o,p,m,n,a[d+6],17,-1473231341),n=e(n,o,p,m,a[d+7],22,-45705983),m=e(m,n,o,p,a[d+8],7,1770035416),p=e(p,m,n,o,a[d+9],12,-1958414417),o=e(o,p,m,n,a[d+10],17,-42063),n=e(n,o,p,m,a[d+11],22,-1990404162),m=e(m,n,o,p,a[d+12],7,1804603682),p=e(p,m,n,o,a[d+13],12,-40341101),o=e(o,p,m,n,a[d+14],17,-1502002290),n=e(n,o,p,m,a[d+15],22,1236535329),m=f(m,n,o,p,a[d+1],5,-165796510),p=f(p,m,n,o,a[d+6],9,-1069501632),o=f(o,p,m,n,a[d+11],14,643717713),n=f(n,o,p,m,a[d],20,-373897302),m=f(m,n,o,p,a[d+5],5,-701558691),p=f(p,m,n,o,a[d+10],9,38016083),o=f(o,p,m,n,a[d+15],14,-660478335),n=f(n,o,p,m,a[d+4],20,-405537848),m=f(m,n,o,p,a[d+9],5,568446438),p=f(p,m,n,o,a[d+14],9,-1019803690),o=f(o,p,m,n,a[d+3],14,-187363961),n=f(n,o,p,m,a[d+8],20,1163531501),m=f(m,n,o,p,a[d+13],5,-1444681467),p=f(p,m,n,o,a[d+2],9,-51403784),o=f(o,p,m,n,a[d+7],14,1735328473),n=f(n,o,p,m,a[d+12],20,-1926607734),m=g(m,n,o,p,a[d+5],4,-378558),p=g(p,m,n,o,a[d+8],11,-2022574463),o=g(o,p,m,n,a[d+11],16,1839030562),n=g(n,o,p,m,a[d+14],23,-35309556),m=g(m,n,o,p,a[d+1],4,-1530992060),p=g(p,m,n,o,a[d+4],11,1272893353),o=g(o,p,m,n,a[d+7],16,-155497632),n=g(n,o,p,m,a[d+10],23,-1094730640),m=g(m,n,o,p,a[d+13],4,681279174),p=g(p,m,n,o,a[d],11,-358537222),o=g(o,p,m,n,a[d+3],16,-722521979),n=g(n,o,p,m,a[d+6],23,76029189),m=g(m,n,o,p,a[d+9],4,-640364487),p=g(p,m,n,o,a[d+12],11,-421815835),o=g(o,p,m,n,a[d+15],16,530742520),n=g(n,o,p,m,a[d+2],23,-995338651),m=h(m,n,o,p,a[d],6,-198630844),p=h(p,m,n,o,a[d+7],10,1126891415),o=h(o,p,m,n,a[d+14],15,-1416354905),n=h(n,o,p,m,a[d+5],21,-57434055),m=h(m,n,o,p,a[d+12],6,1700485571),p=h(p,m,n,o,a[d+3],10,-1894986606),o=h(o,p,m,n,a[d+10],15,-1051523),n=h(n,o,p,m,a[d+1],21,-2054922799),m=h(m,n,o,p,a[d+8],6,1873313359),p=h(p,m,n,o,a[d+15],10,-30611744),o=h(o,p,m,n,a[d+6],15,-1560198380),n=h(n,o,p,m,a[d+13],21,1309151649),m=h(m,n,o,p,a[d+4],6,-145523070),p=h(p,m,n,o,a[d+11],10,-1120210379),o=h(o,p,m,n,a[d+2],15,718787259),n=h(n,o,p,m,a[d+9],21,-343485551),m=b(m,i),n=b(n,j),o=b(o,k),p=b(p,l);return[m,n,o,p]}function j(a){var b,c="";for(b=0;b<32*a.length;b+=8)c+=String.fromCharCode(a[b>>5]>>>b%32&255);return c}function k(a){var b,c=[];for(c[(a.length>>2)-1]=void 0,b=0;b<c.length;b+=1)c[b]=0;for(b=0;b<8*a.length;b+=8)c[b>>5]|=(255&a.charCodeAt(b/8))<<b%32;return c}function l(a){return j(i(k(a),8*a.length))}function m(a,b){var c,d,e=k(a),f=[],g=[];for(f[15]=g[15]=void 0,e.length>16&&(e=i(e,8*a.length)),c=0;16>c;c+=1)f[c]=909522486^e[c],g[c]=1549556828^e[c];return d=i(f.concat(k(b)),512+8*b.length),j(i(g.concat(d),640))}function n(a){var b,c,d="0123456789abcdef",e="";for(c=0;c<a.length;c+=1)b=a.charCodeAt(c),e+=d.charAt(b>>>4&15)+d.charAt(15&b);return e}function o(a){return unescape(encodeURIComponent(a))}function p(a){return l(o(a))}function q(a){return n(p(a))}function r(a,b){return m(o(a),o(b))}function s(a,b){return n(r(a,b))}function t(a,b,c){return b?c?r(b,a):s(b,a):c?p(a):q(a)}"function"==typeof define&&define.amd?define(function(){return t}):a.md5=t}(this);
